00e418136f491912f999f137f798319bc5edc8bd
Jonathan-sm <Jonathan_sm@mail.ru>
2023-11-19 11:30:02 +0300

    исправил тесты

--- src/test/java/pro/sky/KursovayaNumber2/Service/JavaQuestionServiceTest.java
+++ src/test/java/pro/sky/KursovayaNumber2/Service/JavaQuestionServiceTest.java
@@ -13,27 +13,28 @@ import static org.junit.jupiter.api.Assertions.assertTrue;
 
 class JavaQuestionServiceTest {
     JavaQuestionService undetTest = new JavaQuestionService();
-    Question question = new Question("q1", "Q2" + "Q3");
-    Question question2 = new Question("Q4", "Q5" + "Q6");
-    Collection<Question> allQuestion = new HashSet<>(List.of(question, question2));
 
     @Test
      void add_checkAdditionalAnQuestion_returnedQuestion() {
+        Question question = new Question("q1", "Q2" + "Q3");
         Question result = undetTest.add(question);
         assertEquals(question, result);
     }
 
-        @Test
-        void remove_checkRemovedQuestion_questionRemovedAndReturned () {
+     @Test
+    void remove_checkRemovedQuestion_questionRemovedAndReturned () {
+         Question question = new Question("q1", "Q2" );
             Question result = undetTest.remove(question);
             assertEquals(question, result);
-        }
-
-        @Test
-        void getAll_checkGetAllInCollection_returnedGetAll () {
-            undetTest.add(question);
-            undetTest.add(question2);
-            Collection<Question> result = undetTest.getAll();
-            assertTrue(result.containsAll(allQuestion));
-        }
+    }
+    @Test
+    void getAll_checkGetAllInCollection_returnedGetAll () {
+        Question question = new Question("q1", "Q2" + "Q3");
+        Question question2 = new Question("Q4", "Q5" + "Q6");
+        Collection<Question> allQuestion = new HashSet<>(List.of(question, question2));
+          undetTest.add(question);
+          undetTest.add(question2);
+          Collection<Question> result = undetTest.getAll();
+         assertTrue(result.containsAll(allQuestion));
+    }
     }
